b478062a4eff0025581d38b6a1a44ddc9ada5e53
Felix-Dev <felix.ecole@laposte.net>
2025-06-03 15:42:44 +0200

    FIX: Toogle button style and text

--- src/static/JS/toggle-dark-mode.js
+++ src/static/JS/toggle-dark-mode.js
@@ -1,5 +1,5 @@
 document.querySelector("#theme-toggle").onclick = (e) => {
-	e.target.innerText = document.body.classList.contains("dark-mode")
+	e.target.innerText = (!document.body.classList.contains("dark-mode"))
 		? "☀️ Mode clair"
 		: "🌙 Mode sombre";
 
